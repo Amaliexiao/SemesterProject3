7af09e692a92cef13fec43afcbeda31cd97331a9
Asbjoern09 <asbjorn454@gmail.com>
2023-11-05 21:17:53 +0100

    forgot example

--- backend/src/main/java/org/example/OPCUAExample.java
+++ backend/src/main/java/org/example/OPCUAExample.java
@@ -1,2 +1,23 @@
-package org.example;public class OPCUAExample {
+package org.example;
+
+import org.apache.log4j.BasicConfigurator;
+import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+
+import java.util.List;
+
+public class OPCUAExample {
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        //SpringApplication.run(Main.class, args);
+
+        try{
+            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://localhost:4840/milo").get();
+            System.out.println(endpoints);
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
