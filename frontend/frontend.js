d5c82e9c68588629f72a375b9dd0f9c9eaafe11f
SadButCowboy <112869796+SadButCowboy@users.noreply.github.com>
2023-11-18 13:19:54 +0100

    API connection with Inventory
    Sim works with the inventory (malt, yeast, etc.)

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,19 +1,228 @@
 
-function setIngredientAmount() {
-    let barleyValue = "50";
-    let hopsValue = "90";
-    let maltValue = "100";
-    let wheatValue = "50";
-    let yeastValue = "80";
-    document.getElementById("barley")
-        .setAttribute("style", "height:"+barleyValue+"%");
-    document.getElementById("hops")
-        .setAttribute("style", "height:"+hopsValue+"%");
-    document.getElementById("malt")
-        .setAttribute("style", "height:"+maltValue+"%");
-    document.getElementById("wheat")
-        .setAttribute("style", "height:"+wheatValue+"%");
-    document.getElementById("yeast")
-        .setAttribute("style", "height:"+yeastValue+"%");
-}
-setIngredientAmount();
+//Barley
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint you want to call
+    const apiEndpoint = 'http://localhost:8080/fetch/barley';
+
+    // Make the API call using fetch
+    fetch(apiEndpoint)
+        .then(response => {
+            // Check if the request was successful (status code 200)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse the JSON in the response
+            return response.json();
+        })
+        .then(data => {
+            // Access and print the values from the JSON response
+            console.log('API Response:', data);
+
+            // Example: Access a specific value from the JSON
+            const specificValue = data.value; // Replace 'propertyName' with the actual property name in your JSON
+            console.log('Specific Value:', specificValue);
+
+
+            let barleyValue = specificValue/40000*100;
+            // Display the API response on the HTML page
+            document.getElementById('barley').setAttribute("style", "height:"+barleyValue);
+      ;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Display the error on the HTML page
+            document.getElementById('result').innerHTML = `Error: ${error.message}`;
+        });
+});
+
+// Hops
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint you want to call
+    const apiEndpoint = 'http://localhost:8080/fetch/hops';
+
+    // Make the API call using fetch
+    fetch(apiEndpoint)
+        .then(response => {
+            // Check if the request was successful (status code 200)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse the JSON in the response
+            return response.json();
+        })
+        .then(data => {
+            // Access and print the values from the JSON response
+            console.log('API Response:', data);
+
+            // Example: Access a specific value from the JSON
+            const specificValue = data.value; // Replace 'propertyName' with the actual property name in your JSON
+            console.log('Specific Value:', specificValue);
+
+
+            let hopsValue = specificValue/40000*100;
+            // Display the API response on the HTML page
+            document.getElementById('hops').setAttribute("style", "height:"+hopsValue);
+            ;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Display the error on the HTML page
+            document.getElementById('result').innerHTML = `Error: ${error.message}`;
+        });
+});
+
+// Malt
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint you want to call
+    const apiEndpoint = 'http://localhost:8080/fetch/malt';
+
+    // Make the API call using fetch
+    fetch(apiEndpoint)
+        .then(response => {
+            // Check if the request was successful (status code 200)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse the JSON in the response
+            return response.json();
+        })
+        .then(data => {
+            // Access and print the values from the JSON response
+            console.log('API Response:', data);
+
+            // Example: Access a specific value from the JSON
+            const specificValue = data.value; // Replace 'propertyName' with the actual property name in your JSON
+            console.log('Specific Value:', specificValue);
+
+
+            let maltValue = specificValue/40000*100;
+            // Display the API response on the HTML page
+            document.getElementById('malt').setAttribute("style", "height:"+maltValue);
+            ;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Display the error on the HTML page
+            document.getElementById('result').innerHTML = `Error: ${error.message}`;
+        });
+});
+
+// Wheat
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint you want to call
+    const apiEndpoint = 'http://localhost:8080/fetch/wheat';
+
+    // Make the API call using fetch
+    fetch(apiEndpoint)
+        .then(response => {
+            // Check if the request was successful (status code 200)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse the JSON in the response
+            return response.json();
+        })
+        .then(data => {
+            // Access and print the values from the JSON response
+            console.log('API Response:', data);
+
+            // Example: Access a specific value from the JSON
+            const specificValue = data.value; // Replace 'propertyName' with the actual property name in your JSON
+            console.log('Specific Value:', specificValue);
+
+
+            let wheatValue = specificValue/40000*100;
+            // Display the API response on the HTML page
+            document.getElementById('wheat').setAttribute("style", "height:"+wheatValue);
+            ;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Display the error on the HTML page
+            document.getElementById('result').innerHTML = `Error: ${error.message}`;
+        });
+});
+
+// Yeast
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint you want to call
+    const apiEndpoint = 'http://localhost:8080/fetch/yeast';
+
+    // Make the API call using fetch
+    fetch(apiEndpoint)
+        .then(response => {
+            // Check if the request was successful (status code 200)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse the JSON in the response
+            return response.json();
+        })
+        .then(data => {
+            // Access and print the values from the JSON response
+            console.log('API Response:', data);
+
+            // Example: Access a specific value from the JSON
+            const specificValue = data.value; // Replace 'propertyName' with the actual property name in your JSON
+            console.log('Specific Value:', specificValue);
+
+
+            let yeastValue = specificValue/40000*100;
+            // Display the API response on the HTML page
+            document.getElementById('yeast').setAttribute("style", "height:"+yeastValue);
+            ;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Display the error on the HTML page
+            document.getElementById('result').innerHTML = `Error: ${error.message}`;
+        });
+});
+
+// Temperature Sensor
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint you want to call
+    const apiEndpoint = 'http://localhost:8080/fetch/temperatureValue';
+
+    // Make the API call using fetch
+    fetch(apiEndpoint)
+        .then(response => {
+            // Check if the request was successful (status code 200)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse the JSON in the response
+            return response.json();
+        })
+        .then(data => {
+            // Access and print the values from the JSON response
+            console.log('API Response:', data);
+
+            // Example: Access a specific value from the JSON
+            const specificValue = data.value; // Replace 'propertyName' with the actual property name in your JSON
+            console.log('Specific Value:', specificValue);
+
+
+            let temperatureValue = specificValue;
+            // Display the API response on the HTML page
+            document.getElementById('temperature').setAttribute("style", "height:"+temperatureValue)
+            ;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Display the error on the HTML page
+            document.getElementById('result').innerHTML = `Error: ${error.message}`;
+        });
+});
