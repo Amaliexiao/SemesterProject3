da2d26d88ad43763175ca2671e06499056400b15
Asbjørn <asbjorn454@gmail.com>
2023-11-21 17:00:13 +0100

    added authentication, routing and session

--- frontend/login_page/front_page_login_script.js
+++ frontend/login_page/front_page_login_script.js
@@ -1,107 +1,105 @@
 function toggleLoginForm() {
-    const formContainer = document.getElementById('formLoginContainer');
-    const loginForm = document.getElementById('loginForm');
-    const signupForm = document.getElementById('signupForm');
-    const loginButton = document.querySelector('.login-button');
-    const signupButton = document.querySelector('.signup-button');
+  const formContainer = document.getElementById("formLoginContainer");
+  const loginForm = document.getElementById("loginForm");
+  const signupForm = document.getElementById("signupForm");
+  const loginButton = document.querySelector(".login-button");
+  const signupButton = document.querySelector(".signup-button");
 
-    //display login and hide signup
-    loginForm.classList.toggle('active');
-    signupForm.classList.remove('active');
+  //display login and hide signup
+  loginForm.classList.toggle("active");
+  signupForm.classList.remove("active");
 
-    //change display text if other is active
-    signupButton.innerText = 'Signup';
+  //change display text if other is active
+  signupButton.innerText = "Signup";
 
-    // put form in new div container
-    formContainer.innerHTML = '';  // Clear previous content
-    formContainer.appendChild(loginForm);
+  // put form in new div container
+  formContainer.innerHTML = ""; // Clear previous content
+  formContainer.appendChild(loginForm);
 
-    // Change text when clicked
-    loginButton.innerText = loginButton.innerText === 'Login' ? 'Hide' : 'Login';
+  // Change text when clicked
+  loginButton.innerText = loginButton.innerText === "Login" ? "Hide" : "Login";
 }
 
 function toggleSignupForm() {
-    const formContainer = document.getElementById('formSignupContainer');
-    const loginForm = document.getElementById('loginForm');
-    const signupForm = document.getElementById('signupForm');
-    const signupButton = document.querySelector('.signup-button');
-    const loginButton = document.querySelector('.login-button');
+  const formContainer = document.getElementById("formSignupContainer");
+  const loginForm = document.getElementById("loginForm");
+  const signupForm = document.getElementById("signupForm");
+  const signupButton = document.querySelector(".signup-button");
+  const loginButton = document.querySelector(".login-button");
 
-    //display login and hide signup
-    signupForm.classList.toggle('active');
-    loginForm.classList.remove('active');
+  //display login and hide signup
+  signupForm.classList.toggle("active");
+  loginForm.classList.remove("active");
 
-    //change display text if other is active
-    loginButton.innerText = 'Login';
+  //change display text if other is active
+  loginButton.innerText = "Login";
 
-    // put form in new div container
-    formContainer.innerHTML = '';
-    formContainer.appendChild(signupForm);
+  // put form in new div container
+  formContainer.innerHTML = "";
+  formContainer.appendChild(signupForm);
 
-    // Change text when clicked
-    signupButton.innerText = signupButton.innerText === 'Signup' ? 'Hide' : 'Signup';
+  // Change text when clicked
+  signupButton.innerText =
+    signupButton.innerText === "Signup" ? "Hide" : "Signup";
 }
 
 function submitForm(type) {
-    const form = document.getElementById(`${type}Form`);
-    form.classList.remove('active');
+  const form = document.getElementById(`${type}Form`);
+  form.classList.remove("active");
 }
 
 //Auth
 
 async function makeRequest(url, method, data) {
-    try {
-        const response = await fetch(url, {
-            method,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        // Check if the response is JSON
-        const contentType = response.headers.get('content-type');
-        if (contentType && contentType.includes('application/json')) {
-            const result = await response.json();
-            console.log(result);
-            // Handle the JSON response accordingly (redirect, show messages, etc.)
-        } else {
-            // Handle non-JSON response (plain text, etc.)
-            const result = await response.text();
-            console.log(result);
-            // Handle the non-JSON response accordingly
-        }
-    } catch (error) {
-        console.error('Error:', error.message);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+
+    // Check if the response is JSON
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      const result = await response.json();
+      console.log(result);
+      // Handle the JSON response accordingly (redirect, show messages, etc.)
+    } else {
+      // Handle non-JSON response (plain text, etc.)
+      const result = await response.text();
+      console.log(result);
+      // Handle the non-JSON response accordingly
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 }
 
-// Frontend login logic
-const loginForm = document.getElementById('loginForm');
-loginForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
+// // Frontend login logic
+// const loginForm = document.getElementById('loginForm');
+// loginForm.addEventListener('submit', async (event) => {
+//     event.preventDefault();
 
-    const email = document.getElementById('loginEmail').value;
-    const password = document.getElementById('loginPassword').value;
+//     const email = document.getElementById('loginEmail').value;
+//     const password = document.getElementById('loginPassword').value;
 
-    await makeRequest('http://localhost:8080/api/auth/login', 'POST', { email, password });
-});
+//     await makeRequest('http://localhost:8080/api/auth/login', 'POST', { email, password });
+// });
 
-// Frontend signup logic
-const signupForm = document.getElementById('signupForm');
-signupForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
+// // Frontend signup logic
+// const signupForm = document.getElementById('signupForm');
+// signupForm.addEventListener('submit', async (event) => {
+//     event.preventDefault();
 
-    const username = document.getElementById('signupUsername').value;
-    const email = document.getElementById('signupEmail').value;
-    const password = document.getElementById('signupPassword').value;
+//     const username = document.getElementById('signupUsername').value;
+//     const email = document.getElementById('signupEmail').value;
+//     const password = document.getElementById('signupPassword').value;
 
-    await makeRequest('http://localhost:8080/api/auth/signup', 'POST', { username, email, password });
-});
-
-
-
+//     await makeRequest('http://localhost:8080/api/auth/signup', 'POST', { username, email, password });
+// });
--- node-server/server.js
+++ node-server/server.js
@@ -1,14 +1,37 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
-const cors = require('cors');  // Import the 'cors' middleware
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const cors = require("cors"); // Import the 'cors' middleware
 
 const app = express();
 const port = 3000;
-
-// Enable CORS for all routes using middleware
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "Session-key-1891389217",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+const users = [
+  {
+    id: 1,
+    username: "user1@gmail.com",
+    password: "$2b$10$l.i4CzWFj6wS3IiuIZLeDudVWg.bEegPdijwumOCBLZh7ZlRPmNeG",
+  },
+  {
+    id: 2,
+    username: "user2",
+    password: "$2b$10$afVX.Zb0Rc0S1/E.kJ3IBuMl7/q7p3SGHV2pHH2cLCZz7wvL2VYSG",
+  },
+];
 // Parse POST request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,23 +41,64 @@ app.use(express.static(path.join(__dirname, "../frontend/login_page")));
 
 // Serve the login page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/login_page/front_page_login.html"));
+  res.sendFile(
+    path.join(__dirname, "../frontend/login_page/front_page_login.html")
+  );
 });
 
-app.post("/login", (req, res) => {
-  console.log(req.body); // Log the request body
-  const { email, password } = req.body;
-  // Your login logic here
+// Middleware for authentication
+const authenticate = (req, res, next) => {
+  if (req.session && req.session.user) {
+    return next();
+  } else {
+    return res.status(401).send("Unauthorized");
+  }
+};
 
-  // Instead of sending a plain string, send JSON
-  res.json({ message: "Login request received" });
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if the username is valid
+  const user = users.find((u) => u.email === username);
+
+  if (user) {
+    // Compare the provided password with the hashed password in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      // Store user information in the session
+      req.session.user = user;
+      app.use(express.static(path.join(__dirname, "../frontend")));
+      res.redirect("/overview");
+    } else {
+      res.status(401).send("Invalid credentials");
+    }
+  } else {
+    res.status(401).send("Invalid credentials");
+  }
+});
+
+app.get("/overview", authenticate, (req, res) => {
+  const htmlFilePath = path.join(__dirname, "../frontend", "index.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/logout", (req, res) => {
+  // Destroy the session
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send("Logout successful");
+    }
+  });
 });
 
 app.post("/signup", (req, res) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    return res.status(400).json({ error: 'Missing required fields' });
+    return res.status(400).json({ error: "Missing required fields" });
   }
 
   // Your signup logic here
