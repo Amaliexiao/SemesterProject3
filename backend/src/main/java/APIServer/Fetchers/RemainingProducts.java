95b1d2aa94a10f2f5d1e28ff18a8b6c862944eb6
Charlie Warnes <charliewarnes@Charlies-MacBook-Pro-2.local>
2023-12-07 21:03:58 +0100

    test

--- backend/src/main/java/APIServer/Fetchers/RemainingProducts.java
+++ backend/src/main/java/APIServer/Fetchers/RemainingProducts.java
@@ -1,30 +1,35 @@
 package APIServer.Fetchers;
 
+import APIServer.Entities.BatchRepo;
+import APIServer.Entities.Batches;
 import APIServer.OPCUANode;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/fetch")
 public class RemainingProducts extends OPCUANode {
 
-
+    private BatchRepo batchRepo;
     public Variant getTotalProductsValue(){
         Variant totalProductsValue = getNodeValue("ns=6;s=::Program:product.produced");
         return totalProductsValue;
     }
 
-//    public Variant getBatchSize() {
-////        Variant d = null;
-////        return d;
-//    }
-//
-//    @CrossOrigin
-//    @GetMapping("/totalProducts")
-//    public Variant getRemainingProducts() {
-//
-////        return
-//    }
+    @CrossOrigin
+    @PostMapping("/getRemainingProducts/{batchId}")
+    public ResponseEntity<Integer> getBatchSize(@PathVariable Long batchId) {
+        Batches batch = this.batchRepo.findById(batchId).orElse(null);
+
+        if (batch != null) {
+            int batchSize = batch.getSize();
+            int remainingProducts = (Integer) getTotalProductsValue().getValue() - batchSize;
+            return ResponseEntity.ok().body(remainingProducts);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
